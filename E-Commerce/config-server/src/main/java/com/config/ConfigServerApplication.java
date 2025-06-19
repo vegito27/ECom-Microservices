470e89bf4064de910a27fc4883806796cb1b38c9
Your Name <Rishabh.Tripathi@motherson.com>
2025-06-19 17:47:20 +0530

    updated code for spring config

--- E-Commerce/config-server/src/main/java/com/config/ConfigServerApplication.java
+++ E-Commerce/config-server/src/main/java/com/config/ConfigServerApplication.java
@@ -2,8 +2,11 @@ package com.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
